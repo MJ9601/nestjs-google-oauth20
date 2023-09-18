24d001c1b7366a66ad59d17782043fb63e4ae5f1
mohammadjavad khodadadi <m_javad_96@yahoo.com>
2023-09-18 18:24:54 +0330

    feature: adding auth module&controller&service

---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  @Get('google/login')
+  handleLogin() {
+    return { msg: 'google login' };
+  }
+
+
+  @Get("google/redirect")
+  handleRedirect(){
+    return {msg: "OK"}
+  }
+}
